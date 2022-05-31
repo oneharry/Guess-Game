e09e8475c237c7e29dd66911a903f8237edbbf85
Oneharry <60198508+oneharry@users.noreply.github.com>
2022-05-31 23:10:23 +0000

    updated

--- index.js
+++ index.js
@@ -1,41 +1,22 @@
-// const prompt = require('prompt-sync')();
+const prompt = require('prompt-sync')();
+const Cookies = require('js-cookie')
 
 
 
 const guessGame = (min = 1, max = 2) => {
-
-    //sets cookie
-    const setCookie = (userName, stage) => {
-        document.cookie = `username=${userName}, stage=${stage}`
-    }
-    
-    const getCookie = (userName) => {
-        const name = `${username}=`;
-        const decoded = decodeURIComponent(document.cookie);
-        const cookieArr = decoded.split(';');
-        let value;
-        cookieArr.forEach(x => {
-            if(x.indexOf(name) === 0) {
-                value = x.substring(name.length);
-    
-            } else {
-                value = "";
-            }
-        })
-        return value;
-    }
-    
-
+  
     //gets random numbers
     const getRandomNum = (max, min) => {
         
-        return (Math.random() * (max-min) + min).toFixed(1);
+        return (Math.random() * (max-min) +min).toFixed(1);     
         
     }
    
     //prompts user for username
     let userName = prompt("Enter Username> ");
-
+    // let expires = new Date()
+     Cookies.set(userName, 'hiui', {expires: 7})
+      console.log(Cookies.get)
     //only usernames can play
     if(userName) {
         console.log(userName)
@@ -43,10 +24,11 @@ const guessGame = (min = 1, max = 2) => {
     
         
         let randomNumber = getRandomNum(max, min);
-        setCookie(userName, stage)
+       
 
         //prompts for number
         let userGuess = Number(prompt("Predict number to one decimal place> "));
+      console.log(Cookies.get('userName'))
 
         //checks that users input is within range
         if(userGuess < min || userGuess > max) {
@@ -58,7 +40,7 @@ const guessGame = (min = 1, max = 2) => {
                 max++;
                 stage++;
                 
-                setCookie(userName, stage)
+                console.log(Cookies.get(userName))
                 console.log("stage", stage, max)
                 console.log("Congratulation!!! .... Next level")
             
